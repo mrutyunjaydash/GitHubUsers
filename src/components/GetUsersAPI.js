665fbf9b4bb1a533f1ea8cb63458a09d85d619cc
Mrutyunjay Dash <mdash555@gmail.com>
2021-07-15 13:36:25 +0530

    loader added and code refactored

--- src/components/GetUsersAPI.js
+++ src/components/GetUsersAPI.js
@@ -1,18 +1,21 @@
 import React, { useEffect, useState } from 'react';
-import { FcApproval } from "react-icons/fc";
-import { FaGithub } from "react-icons/fa";
+import Loader from './Loader';
+import UsersPage from './UsersPage';
 
 const GetUsersAPI = () => {
     const [users,setUsers] = useState([]);
+    const [loading,setLoading] = useState(true);
 
     const getUsers = async() =>{
         try{
                     
             const response = await fetch("https://api.github.com/users");
+            setLoading(false);
             setUsers(await response.json());
         }
         catch(error)
         {
+            setLoading(false);
             console.log(error);
         }
     }
@@ -21,46 +24,14 @@ const GetUsersAPI = () => {
         getUsers();
     },[]);
 
+    if(loading){
+        return(
+            <Loader />
+        )
+    }
+
     return (
-        <>
-          <h2>List of GitHub Users</h2>
-            <div className="container-fluid mt-5">
-                <div className="row text-center">
-                    
-                {
-
-                        users.map((curElem) => {
-                        
-                            const { avatar_url, id, login, type } = curElem;
-                        return (
-                              <div className="col-10 col-md-4 mt-5" key={id}>
-                      <div className="card p-2">
-                                <div className="d-flex align-items-center">
-                                        <div className="image"> <img src={ avatar_url } className="rounded" width="155" /> </div>
-                                    <div className="ml-3 w-100">
-                                            <h4 className="mb-0 mt-0 textLeft">{login} <FcApproval/> </h4>
-                                            {/* <span className="text-left">{type }</span> */}
-                                        <div className="p-2 mt-2 bg-light d-flex justify-content-between rounded stats">
-                                                <div className="d-flex flex-column">
-                                                    <span className="articles">Articles</span> <span className="number1">38</span> </div>
-                                                <div className="d-flex flex-column">
-                                                    <span className="followers">Followers</span> <span className="number2">980</span> </div>
-                                                <div className="d-flex flex-column">
-                                                    <span className="rating">Rating</span> <span className="number3">8.9</span> </div>
-                                        </div>
-                                      
-                                    </div>
-                             </div>
-                          </div>
-                    </div>
-                        )
-                })        
-
-                }
-                    
-                </div>
-            </div>
-        </>
+       <UsersPage users={users} />
     );
 };
 
